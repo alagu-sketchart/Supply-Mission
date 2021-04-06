01bbc12d7be5b1fbf1566d0fd3eaf2b4cb3b1b33
alagu-sketchart <74680709+alagu-sketchart@users.noreply.github.com>
2021-04-05 20:18:39 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,69 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody, ground;
+const Body = Matter.Bodies;
+const World = Matter.World;
+
+var helicopterIMG, helicopterSprite, packageSprite ,packageIMG;
+var world, engine;
+var ground, packageBody;
 
 function preload(){
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+helicopterIMG = loadImage("helicopter.png");
+packageIMG = loadImage("package.png");
 }
 
-function setup() {
-	createCanvas(800, 700);
-	//rectMode(CENTER);
-	engine = Engine.create();
-	world = engine.world;
-	
-	packageSprite=createSprite(width/2, 200, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.3
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(155)
-
-
-	packageBody = Bodies.circle(width/2 , 200 , 5, {restitution: 3.5, isStatic:true});
-	//packageBody = Bodies.rectangle(width/2, 200, 5, {restitution: 2, isStatic: true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	rectMode(CENTER)
-	ground = Bodies.rectangle(width/2, 650, width, 10, {isStatic: true} );
- 	World.add(world, ground, packageBody);
-
-
-	Engine.run(engine);
-}
-
-
-function draw() {
+function setup(){
+  createCanvas(700,800);
   rectMode(CENTER);
+  engine = Engine.create();
+  world = engine.world;
+
+  packageSprite = createSprite(width/2, 100, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale = 0.2;
+
+	helicopterSprite = createSprite(width/2, 100, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+  helicopterSprite.scale = 0.6
+  
+  ground = createSprite(width/2, height-150, width, 20);
+  ground.shapeColor = color(255);
+
+var ground_p = {
+    isStatic : true 
+}
+
+  ground = Bodies.rectangle(width/2, 650, 100, 40, ground_p);
+  World.add(world, ground);
+
+  var packageBody_p = {
+    restitution: 0.5 
+}
+
+packageBody = Bodies.circle(width/2,100,20,packageBody_p);
+}
+
+function draw(){
   background(0);
   Engine.update(engine);
+  packageSprite.y = packageBody.position.y
+  
+  rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
 
-  packageSprite.x= packageBody.position.x 
+    ellipseMode(RADIUS);
+    ellipse(packageBody.position.x, packageBody.position.y, 20, 20);
   drawSprites();
- 
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-
-	packageSprite.velocityY = 4;
-	packageBody.velocityY = 4;
-	
-	background(105);
-	
-  }
-}
+  if (keyCode === DOWN_ARROW) {
+     //Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+   
+     var packageBody_p = {
+     restitution: 0.5
+   }
+   packageBody = Bodies.circle(width/2,100,20,packageBody_p);
+   World.add(world,packageBody);
+   }
+ }
